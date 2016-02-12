29789c65662339ee6e0b5f61ae766df4e9f1f138
David Nahm <David@airbears2-10-142-69-178.airbears2.1918.berkeley.edu>
2016-02-11 23:44:59 -0800

    finished hw #1

--- foobar.rb
+++ foobar.rb
@@ -1,5 +1,10 @@
 class Foobar
-	def baz(a)
-		a.map!(x to_i(x))
+	def self.baz(a)
+		a = a.map{|x| x.to_i}
+		a = a.map{|x| x+2}
+		a = a.select{|x| x.even?}
+		a.uniq!
+		a = a.select{|x| x <= 10}
+		a = a.inject(0){|sum, x| sum + x}
 	end
 end
